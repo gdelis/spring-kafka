469e8644780c177a5185a28a95f936403d86ab30
George Delis <gdelis1989@gmail.com>
2024-07-11 00:36:17 +0100

    Implement Avro serializer and deserializer

--- src/main/java/com/gdelis/spring/kafka/Application.java
+++ src/main/java/com/gdelis/spring/kafka/Application.java
@@ -1,37 +1,88 @@
 package com.gdelis.spring.kafka;
 
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.avro.Schema;
+import org.apache.avro.generic.GenericRecord;
+import org.apache.avro.generic.GenericRecordBuilder;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.context.annotation.DependsOn;
 
 @SpringBootApplication
 public class Application {
+
+   @Value("${kafka.users.topic}")
+   private String topic;
 
    public static void main(String[] args) {
       SpringApplication.run(Application.class, args);
    }
 
-   @KafkaListener(id = "salute-consumer", topics = "users")
-   public void salute(String user) {
-      System.out.println("Hello user: " + user);
+   @Bean
+   public ApplicationRunner producerRunner(
+       @Qualifier("userKafkaProducer") KafkaProducer<String, GenericRecord> producer,
+       @Qualifier("userAvroSchema") Schema userAvroSchema) {
+
+      return args -> {
+         List<GenericRecord> genericRecords = generateRecords(userAvroSchema);
+
+         genericRecords.stream()
+                       .map(s -> new ProducerRecord<String, GenericRecord>("users", null, s))
+                       .forEach(record -> producer.send(record, (recordMetadata, e) -> {
+                          System.out.println("recordMetadata = " + recordMetadata.toString());
+
+                          if (e != null) {
+                             System.out.println(e.getMessage());
+                          }
+                       }));
+      };
    }
 
    @Bean
-   public ApplicationRunner runner(@Qualifier("stringKafkaProducer") KafkaProducer<String, String> producer) {
+   @DependsOn("producerRunner")
+   public ApplicationRunner consumerRunner(
+       @Qualifier("userKafkaConsumer") KafkaConsumer<String, GenericRecord> consumer) {
+      return args -> {
+         consumer.subscribe(List.of(topic));
 
-      ProducerRecord<String, String> record = new ProducerRecord<>("users", "name", "George");
-
-      return args -> producer.send(record, (recordMetadata, e) -> {
-         System.out.println("recordMetadata = " + recordMetadata);
-
-         if (e != null) {
-            System.out.println(e.getMessage());
+         try {
+            while (true) {
+               ConsumerRecords<String, GenericRecord> records = consumer.poll(100);
+               for (ConsumerRecord<String, GenericRecord> r : records) {
+                  System.out.printf("offset = %d, key = %s, value = %s \n", r.offset(), r.key(),
+                                    r.value());
+               }
+            }
+         } finally {
+            consumer.close();
          }
-      });
+      };
+   }
+
+   private List<GenericRecord> generateRecords(final Schema userAvroSchema) {
+      List<GenericRecord> records = new ArrayList<>();
+
+      GenericRecordBuilder genericRecordBuilder = new GenericRecordBuilder(userAvroSchema);
+
+      for (int i = 0; i < 10; i++) {
+         genericRecordBuilder.set("firstName", "George-" + i);
+         genericRecordBuilder.set("lastName", "Delis-" + i);
+         genericRecordBuilder.set("telephone", "222-222-2222-" + i);
+
+         records.add(genericRecordBuilder.build());
+      }
+
+      return records;
    }
 }
+
--- src/main/java/com/gdelis/spring/kafka/configuration/KafkaConfiguration.java
+++ src/main/java/com/gdelis/spring/kafka/configuration/KafkaConfiguration.java
@@ -1,14 +1,25 @@
 package com.gdelis.spring.kafka.configuration;
 
 import java.util.Properties;
+import org.apache.avro.Schema;
+import org.apache.avro.SchemaBuilder;
+import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.TopicBuilder;
 
 @Configuration
 public class KafkaConfiguration {
+
+   @Value("${kafka.users.topic}")
+   private String topic;
 
    /**
     * Causes the topic to be created on the broker; it is not needed if the topic already exists.
@@ -17,21 +28,67 @@ public class KafkaConfiguration {
     */
    @Bean
    public NewTopic users() {
-      return TopicBuilder.name("users")
+      return TopicBuilder.name(topic)
                          .partitions(1)
                          .replicas(1)
                          .build();
    }
 
    @Bean
-   public KafkaProducer<String, String> stringKafkaProducer() {
+   public Properties kafkaProducerProperties() {
       Properties kafkaProperties = new Properties();
 
-      kafkaProperties.put("bootstrap.servers", "localhost:9092");
-      kafkaProperties.put("client.id", "spring-kafka");
-      kafkaProperties.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
-      kafkaProperties.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
+      kafkaProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+      kafkaProperties.put(ProducerConfig.CLIENT_ID_CONFIG, "spring-kafka");
+      kafkaProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG,
+                          "io.confluent.kafka.serializers.KafkaAvroSerializer");
+      kafkaProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,
+                          "io.confluent.kafka.serializers.KafkaAvroSerializer");
+      kafkaProperties.put("schema.registry.url", "http://localhost:8081");
 
-      return new KafkaProducer<>(kafkaProperties);
+      return kafkaProperties;
+   }
+
+   @Bean
+   public Properties kafkaConsumerProperties() {
+      Properties kafkaProperties = new Properties();
+
+      kafkaProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+      kafkaProperties.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+      kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG,
+                          "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+      kafkaProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG,
+                          "io.confluent.kafka.serializers.KafkaAvroDeserializer");
+      kafkaProperties.put("schema.registry.url", "http://localhost:8081");
+      kafkaProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+      return kafkaProperties;
+   }
+
+   @Bean
+   public Schema userAvroSchema() {
+      return SchemaBuilder.record("User")
+                          .fields()
+                          .requiredString("firstName")
+                          .requiredString("lastName")
+                          .name("telephone")
+                          .type()
+                          .nullable()
+                          .stringType()
+                          .noDefault()
+                          .endRecord();
+   }
+
+   @Bean
+   public KafkaProducer<String, GenericRecord> userKafkaProducer(
+       @Qualifier("kafkaProducerProperties") final Properties producerProperties) {
+
+      return new KafkaProducer<>(producerProperties);
+   }
+
+   @Bean
+   public KafkaConsumer<String, GenericRecord> userKafkaConsumer(
+       @Qualifier("kafkaConsumerProperties") final Properties consumerProperties) {
+      return new KafkaConsumer<>(consumerProperties);
    }
 }
