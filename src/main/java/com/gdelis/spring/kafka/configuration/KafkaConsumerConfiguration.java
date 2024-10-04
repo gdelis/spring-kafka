7902ce2ec679abac2cf5870e4d1852aa1796946f
George Delis <gdelis1989@gmail.com>
2024-10-04 21:34:47 +0100

    Create multiple consumers in the same consumer group

--- src/main/java/com/gdelis/spring/kafka/configuration/KafkaConsumerConfiguration.java
+++ src/main/java/com/gdelis/spring/kafka/configuration/KafkaConsumerConfiguration.java
@@ -1,15 +1,25 @@
 package com.gdelis.spring.kafka.configuration;
 
+import com.gdelis.spring.kafka.CountryEnum;
+import com.gdelis.spring.kafka.UserDetails;
+import com.gdelis.spring.kafka.UserTypeEnum;
+import com.gdelis.spring.kafka.interceptor.HeadersConsumerInterceptor;
+import com.gdelis.spring.kafka.repository.UserDetailsRepository;
 import io.confluent.kafka.serializers.KafkaAvroDeserializer;
+import java.time.Duration;
+import java.util.List;
 import java.util.Properties;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
 
 @Configuration
 public class KafkaConsumerConfiguration {
@@ -23,7 +33,7 @@ public class KafkaConsumerConfiguration {
       kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
       //kafkaProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
       kafkaProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, KafkaAvroDeserializer.class.getName());
-      //kafkaProperties.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, HeadersConsumerInterceptor.class.getName());
+      kafkaProperties.put(ConsumerConfig.INTERCEPTOR_CLASSES_CONFIG, HeadersConsumerInterceptor.class.getName());
       kafkaProperties.put("schema.registry.url", "http://localhost:8081");
       kafkaProperties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
       
@@ -31,8 +41,61 @@ public class KafkaConsumerConfiguration {
    }
    
    @Bean
+   @DependsOn("usersProducerRunner")
    KafkaConsumer<String, GenericRecord> usersKafkaConsumer(
-       @Qualifier("usersKafkaConsumerProperties") final Properties consumerProperties) {
+       @Qualifier("usersKafkaConsumerProperties") final Properties consumerProperties,
+       @Value("${kafka.users.topic}") final String topic,
+       final UserDetailsRepository userDetailsRepository) {
+      
+      for (int i = 0; i < 3; i++) {
+         new Thread(() -> {
+            KafkaConsumer<String, GenericRecord> consumer = new KafkaConsumer<>(consumerProperties);
+            consumer.subscribe(List.of(topic));
+            
+            while (true) {
+               ConsumerRecords<String, GenericRecord> records = consumer.poll(Duration.ofMillis(100));
+               for (ConsumerRecord<String, GenericRecord> record : records) {
+                  System.out.printf("Thread %s consumed record with key %s and value %s%n",
+                                    Thread.currentThread()
+                                          .getId(),
+                                    record.key(),
+                                    record.value());
+                  
+                  UserDetails userDetails = userDetailsConverter(record);
+                  userDetailsRepository.save(userDetails);
+               }
+            }
+         }).start();
+      }
+      
       return new KafkaConsumer<>(consumerProperties);
+   }
+   
+   private UserDetails userDetailsConverter(final ConsumerRecord<String, GenericRecord> r) {
+      CountryEnum country = CountryEnum.getCountryEnumFromAbbreviationValue(r.value()
+                                                                             .get("country")
+                                                                             .toString());
+      UserTypeEnum type = UserTypeEnum.valueOf(r.value()
+                                                .get("type")
+                                                .toString());
+      
+      return new UserDetails(r.value()
+                              .get("username")
+                              .toString(),
+                             r.value()
+                              .get("firstName")
+                              .toString(),
+                             r.value()
+                              .get("lastName")
+                              .toString(),
+                             r.value()
+                              .get("email")
+                              .toString(),
+                             r.value()
+                              .get("telephone")
+                              .toString(),
+                             country,
+                             type,
+                             null);
    }
 }
--- src/main/java/com/gdelis/spring/kafka/configuration/KafkaMessagesGenerator.java
+++ src/main/java/com/gdelis/spring/kafka/configuration/KafkaMessagesGenerator.java
@@ -1,11 +1,8 @@
 package com.gdelis.spring.kafka.configuration;
 
 import com.gdelis.spring.kafka.CountryEnum;
-import com.gdelis.spring.kafka.UserDetails;
 import com.gdelis.spring.kafka.UserTypeEnum;
-import com.gdelis.spring.kafka.repository.UserDetailsRepository;
 import java.nio.charset.StandardCharsets;
-import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
@@ -13,9 +10,6 @@ import org.apache.avro.Schema;
 import org.apache.avro.generic.GenericData;
 import org.apache.avro.generic.GenericRecord;
 import org.apache.avro.generic.GenericRecordBuilder;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -23,11 +17,18 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.DependsOn;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaMessagesGenerator {
+   
+   private final Callback genericCallback = (recordMetadata, e) -> {
+      System.out.println("recordMetadata = " + recordMetadata.toString());
+      
+      if (e != null) {
+         System.out.println(e.getMessage());
+      }
+   };
    
    @Bean
    public ApplicationRunner usersProducerRunner(
@@ -35,19 +36,11 @@ public class KafkaMessagesGenerator {
        @Qualifier("usersAvroSchema") Schema userAvroSchema,
        @Value("${kafka.users.topic}") String topic) {
       
-      Callback genericCallback = (recordMetadata, e) -> {
-         System.out.println("recordMetadata = " + recordMetadata.toString());
-         
-         if (e != null) {
-            System.out.println(e.getMessage());
-         }
-      };
-      
       return args -> {
          List<GenericRecord> genericRecords = generateRecords(userAvroSchema);
          
          genericRecords.stream()
-                       .map(s -> new ProducerRecord<String, GenericRecord>(topic, s))
+                       .map(s -> new ProducerRecord<>(topic, getKafkaMessageKey(s), s))
                        .forEach(record -> {
                           record.headers()
                                 .add("producer-header-author", "George Delis".getBytes(StandardCharsets.UTF_8));
@@ -56,12 +49,20 @@ public class KafkaMessagesGenerator {
       };
    }
    
+   private String getKafkaMessageKey(final GenericRecord s) {
+      if (s.get("username") != null) {
+         return (String) s.get("username");
+      }
+      
+      return "random";
+   }
+   
    private List<GenericRecord> generateRecords(final Schema userAvroSchema) {
       List<GenericRecord> records = new ArrayList<>();
       
       GenericRecordBuilder genericRecordBuilder = new GenericRecordBuilder(userAvroSchema);
       
-      for (int i = 10; i < 20; i++) {
+      for (int i = 0; i < 20; i++) {
          genericRecordBuilder.set("username", "george/delis-" + i);
          genericRecordBuilder.set("firstName", "George-" + i);
          genericRecordBuilder.set("lastName", "Delis-" + i);
