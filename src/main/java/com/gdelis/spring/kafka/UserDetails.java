34352a605b014cbbe21b09c273326d37958733b5
George Delis <gdelis1989@gmail.com>
2024-10-06 11:10:12 +0100

    Update project dependencies

--- src/main/java/com/gdelis/spring/kafka/UserDetails.java
+++ src/main/java/com/gdelis/spring/kafka/UserDetails.java
@@ -4,6 +4,11 @@ import com.gdelis.spring.kafka.utils.Telephone;
 import jakarta.validation.constraints.Email;
 import java.util.Map;
 
-public record UserDetails(String username, String firstName, String lastName, @Email String email,
-                          @Telephone String telephone, CountryEnum country, UserTypeEnum type,
+public record UserDetails(String username,
+                          String firstName,
+                          String lastName,
+                          @Email String email,
+                          @Telephone String telephone,
+                          CountryEnum country,
+                          UserTypeEnum type,
                           Map<String, String> details) {}
