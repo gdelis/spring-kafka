ef7220a05f3cd9f705545cfa0755382f2b0e75eb
George Delis <gdelis1989@gmail.com>
2024-10-19 23:58:21 +0100

    Extend exchange interface

--- src/main/java/com/gdelis/spring/kafka/configuration/HttpServiceConfiguration.java
+++ src/main/java/com/gdelis/spring/kafka/configuration/HttpServiceConfiguration.java
@@ -1,6 +1,7 @@
 package com.gdelis.spring.kafka.configuration;
 
 import com.gdelis.spring.kafka.controller.ProductsHttpService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestClient;
@@ -16,8 +17,10 @@ public class HttpServiceConfiguration {
    }
    
    @Bean
-   public HttpServiceProxyFactory httpServiceProxyFactory(final RestClient.Builder builder) {
-      RestClient client = builder.baseUrl("http://localhost:9000")
+   public HttpServiceProxyFactory httpServiceProxyFactory(
+       @Value("${services.products.url:http://localhost:9000}") final String baseUrl,
+       final RestClient.Builder builder) {
+      RestClient client = builder.baseUrl(baseUrl)
                                  .build();
       
       return HttpServiceProxyFactory.builderFor(RestClientAdapter.create(client))
