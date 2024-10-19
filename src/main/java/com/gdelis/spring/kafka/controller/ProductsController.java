2efda39aed576d40d18144b7f772e7b0711bbd71
George Delis <gdelis1989@gmail.com>
2024-10-19 23:58:21 +0100

    Extend exchange interface

--- src/main/java/com/gdelis/spring/kafka/controller/ProductsController.java
+++ src/main/java/com/gdelis/spring/kafka/controller/ProductsController.java
@@ -3,7 +3,11 @@ package com.gdelis.spring.kafka.controller;
 import com.gdelis.spring.kafka.Product;
 import java.util.List;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +24,15 @@ public class ProductsController {
    @GetMapping
    ResponseEntity<List<Product>> getProducts() {
       return productsHttpService.getProducts();
+   }
+   
+   @PostMapping
+   ResponseEntity<Product> createProduct(@RequestBody Product product) {
+      return productsHttpService.addProduct(product);
+   }
+   
+   @DeleteMapping("/{id}")
+   ResponseEntity<Void> deleteProduct(@PathVariable Integer id) {
+      return productsHttpService.deleteProduct(id);
    }
 }
