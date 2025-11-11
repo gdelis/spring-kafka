3ec3aaeb71bb2b1e6ca0643d36636ba438dcc34a
George Delis <gdelis1989@gmail.com>
2025-11-11 11:32:51 +0200

    Refactor topics

--- src/main/java/com/gdelis/spring/kafka/configuration/KafkaTopicConfiguration.java
+++ src/main/java/com/gdelis/spring/kafka/configuration/KafkaTopicConfiguration.java
@@ -15,7 +15,8 @@ public class KafkaTopicConfiguration {
     * @return
     */
    @Bean
-   NewTopic usersSink(@Value("${kafka.users.topic.partitions}") final Integer partitions, @Value("${kafka.users.topic.sink.name}") final String name) {
+   NewTopic usersSink(@Value("${kafka.users.topic.partitions}") final Integer partitions,
+                      @Value("${kafka.users.topic.sink.name}") final String name) {
       return TopicBuilder.name(name)
                          .partitions(partitions)
                          .replicas(1)
@@ -23,7 +24,17 @@ public class KafkaTopicConfiguration {
    }
    
    @Bean
-   NewTopic usersSource(@Value("${kafka.users.topic.partitions}") final Integer partitions, @Value("${kafka.users.topic.source.name}") final String name) {
+   NewTopic usersSource(@Value("${kafka.users.topic.partitions}") final Integer partitions,
+                        @Value("${kafka.users.topic.source.name}") final String name) {
+      return TopicBuilder.name(name)
+                         .partitions(partitions)
+                         .replicas(1)
+                         .build();
+   }
+   
+   @Bean
+   NewTopic addressesSource(@Value("${kafka.addresses.topic.partitions}") final Integer partitions,
+                            @Value("${kafka.addresses.topic.name}") final String name) {
       return TopicBuilder.name(name)
                          .partitions(partitions)
                          .replicas(1)
